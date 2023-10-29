183cd50bb48b83abacc02936833824e83ba300ea
danielnachun <daniel.nachun@gmail.com>
2023-10-28 20:42:15 -0700

    fix cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 make LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR-}" != "" ]]; then
-make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
+    make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
 fi
 make install LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
 rm -f ${PREFIX}/lib/libunibilium.a
